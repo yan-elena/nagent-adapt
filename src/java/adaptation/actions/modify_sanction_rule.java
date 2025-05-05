c477021838140d123c928b3d9c49f2c0526710dd
Elena <elena.yan@emse.fr>
2025-05-06 00:08:48 +0200

    feat: modify sanction rule

---
+++ src/java/adaptation/actions/modify_sanction_rule.java
@@ -0,0 +1,43 @@
+package adaptation.actions;
+
+import adaptation.agent.ANormativeAgent;
+import jason.JasonException;
+import jason.asSemantics.DefaultInternalAction;
+import jason.asSemantics.TransitionSystem;
+import jason.asSemantics.Unifier;
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+import jason.asSyntax.Term;
+
+/**
+ * An internal action for modify an existing norm in the normative engine of the agent.
+ * The following parameters are required:
+ * +id : String, the id of the norm to be modified
+ * +condition : LogicalFormula, the condition of the modified norm
+ * +consequence : Literal, the consequence of the modified norm
+ */
+public class modify_sanction_rule extends DefaultInternalAction {
+
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        try {
+            ANormativeAgent ag = (ANormativeAgent) ts.getAg();
+            Literal id = (Literal) args[0];
+            LogicalFormula condition = (LogicalFormula) args[1];
+            Literal consequence = (Literal) args[2];
+
+            ag.getLogger().info("[Action] Modify sanction rule - id: " + id + " with condition: " + condition + " consequence: " + consequence);
+
+            ag.getNPLAInterpreter().modifySanctionRule(id, condition, consequence);
+            ag.updateSpecification();
+            ag.getNPLAInterpreter().verifyNorms();
+            return true;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new JasonException("The internal action 'modify_sanction_rule'" + "has not received three arguments!");
+        } catch (ClassCastException e) {
+            throw new JasonException("The internal action 'modify_sanction_rule" + "has received arguments with the wrong type!");
+        } catch (Exception e) {
+            throw new JasonException("Error in 'modify_sanction_rule'");
+        }
+    }
+}
