28410b967685391fd9485d1cdcde8fca9a3bab97
Elena <elena.yan@emse.fr>
2025-05-06 00:03:27 +0200

    refactor: add sanction action

--- src/java/adaptation/actions/add_sanction_rule.java
+++ src/java/adaptation/actions/add_sanction_rule.java
@@ -25,15 +25,15 @@ public class add_sanction_rule extends DefaultInternalAction {
             Literal id = (Literal) args[0];
             LogicalFormula condition = (LogicalFormula) args[1];
             Literal consequence = (Literal) args[2];
-            ag.getLogger().info("[Action] Add new norm - id: " + id + " condition: " + condition + " consequence: " + consequence);
+            ag.getLogger().info("[Action] Add new sanction rule - id: " + id + " condition: " + condition + " consequence: " + consequence);
             ag.getNPLAInterpreter().addSanctionRule(id, condition, consequence);
             return true;
         } catch (ArrayIndexOutOfBoundsException e) {
-            throw new JasonException("The internal action 'add_norm'" + "has not received three arguments!");
+            throw new JasonException("The internal action 'add_sanction_rule'" + "has not received three arguments!");
         } catch (ClassCastException e) {
-            throw new JasonException("The internal action 'add_norm" + "has received arguments with the wrong type!");
+            throw new JasonException("The internal action 'add_sanction_rule" + "has received arguments with the wrong type!");
         } catch (Exception e) {
-            throw new JasonException("Error in 'add_norm'");
+            throw new JasonException("Error in 'add_sanction_rule'");
         }
     }
 }
