e39b5bb3aa31f1b9c652922a125e4d9be775b96c
Elena <elena.yan@emse.fr>
2025-05-05 23:13:19 +0200

    feat: remove norm

--- src/java/adaptation/actions/remove_norm.java
+++ src/java/adaptation/actions/remove_norm.java
@@ -1,6 +1,7 @@
 package adaptation.actions;
 
 import adaptation.agent.ANormativeAgent;
+import jason.JasonException;
 import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
@@ -9,15 +10,25 @@ import jason.asSyntax.Term;
 
 /**
  * An internal action for remove an existing norm in the normative engine of the agent.
+ * The following parameters are required:
+ * +id : String, the id of the existing norm to be removed
  */
 public class remove_norm extends DefaultInternalAction {
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-        ANormativeAgent ag = (ANormativeAgent) ts.getAg();
-        StringTerm id = (StringTerm) args[0];
-        ag.getLogger().info("[Action] Remove norm - id: " + id);
-        ag.getNPLAInterpreter().removeNorm(id.getString());
-        return true;
+        try {
+            ANormativeAgent ag = (ANormativeAgent) ts.getAg();
+            StringTerm id = (StringTerm) args[0];
+            ag.getLogger().info("[Action] Remove norm - id: " + id);
+            ag.getNPLAInterpreter().removeNorm(id.getString());
+            return true;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new JasonException("The internal action 'remove_norm'" + "has not received three arguments!");
+        } catch (ClassCastException e) {
+            throw new JasonException("The internal action 'remove_norm" + "has received arguments with the wrong type!");
+        } catch (Exception e) {
+            throw new JasonException("Error in 'remove_norm'");
+        }
     }
 }
